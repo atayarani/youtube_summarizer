967d11413c8d708da67664d021fa1859fd3475b7
Ali <619512+atayarani@users.noreply.github.com>
2024-01-10 06:03:12 +0000

    clean

--- transcript.py
+++ transcript.py
@@ -5,12 +5,19 @@ from langchain_community.document_loaders import YoutubeLoader
 
 @dataclass
 class Metadata:
+    """
+    Represents the metadata of a transcript.
+    """
+
     title: str
     publish_date: str
     author: str
     url: str
 
     def print(self) -> None:
+        """
+        Prints the metadata information.
+        """
         print(f"Title: {self.title}")
         print(f"Publish Date: {self.publish_date}")
         print(f"Author: {self.author}")
@@ -19,11 +26,24 @@ class Metadata:
 
 @dataclass
 class Transcript:
+    """
+    Represents a transcript.
+    """
+
     content: str
     metadata: Metadata
 
     @classmethod
     def get_transcript(cls, url: str) -> "Transcript":
+        """
+        Retrieves a transcript from the given URL.
+
+        Args:
+            url (str): The URL of the transcript.
+
+        Returns:
+            Transcript: The retrieved transcript.
+        """
         loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
         output = loader.load()[0]
         metadata = Metadata(
