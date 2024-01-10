bb788d9dffd7e5dee46fc989dcc4c5ed7ae967e3
Ali <619512+atayarani@users.noreply.github.com>
2024-01-10 05:49:51 +0000

    Convert script to CLI app

---
+++ cli.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from transcript import Transcript
+from ai import AI
+import click
+
+
+@click.command()
+@click.option(
+    "--url",
+    prompt="The URL of the YouTube video.",
+    help="The URL of the YouTube video.",
+)
+def main(url: str):
+    """
+    This function takes a URL as input and performs the following steps:
+    1. Retrieves the transcript for the given URL.
+    2. Initializes an AI object with the transcript content.
+    3. Prints the AI's output.
+    4. Prints the metadata of the transcript.
+    
+    Args:
+        url (str): The URL of the YouTube video.
+    """
+    transcript = Transcript.get_transcript(url)
+    ai = AI(transcript.content)
+    ai.print()
+    transcript.metadata.print()
+
+
+if __name__ == "__main__":
+    main()
---
+++ transcript.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from langchain_community.document_loaders import YoutubeLoader
+
+
+@dataclass
+class Metadata:
+    title: str
+    publish_date: str
+    author: str
+    url: str
+
+    def print(self) -> None:
+        print(f"Title: {self.title}")
+        print(f"Publish Date: {self.publish_date}")
+        print(f"Author: {self.author}")
+        print(f"URL: {self.url}")
+
+@dataclass
+class Transcript:
+    content: str
+    metadata: Metadata
+
+    @classmethod
+    def get_transcript(cls, url: str) -> "Transcript":
+        loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
+        output = loader.load()[0]
+        metadata = Metadata(
+            title=output.metadata["title"],
+            publish_date=output.metadata["publish_date"],
+            author=output.metadata["author"],
+            url=url,
+        )
+
+        return cls(content=output.page_content, metadata=metadata)
+    
+
