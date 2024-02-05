763df6f1c6989835743e6b1fc9833dd4224fd9dc
ChronoSerrano <619512+ChronoSerrano@users.noreply.github.com>
2024-02-05 02:53:05 -0800

    fix tests

--- tests/cli/test_get_transcript.py
+++ tests/cli/test_get_transcript.py
@@ -14,7 +14,7 @@ def test_function_succeeds(mocker):
 
 @pytest.mark.parametrize(
     "get_transcript_return_value",
-    [pytest.param((1, "foo"), id="not_found"), pytest.param((2, "foo"), id="disabled")],
+    [pytest.param((1, "foo"), id="not_found")],
 )
 def test_generation_succeeds(mocker, get_transcript_return_value):
     mock_transcript_get_transcript = mocker.patch("src.transcript.get_transcript")
@@ -45,38 +45,6 @@ def test_generated_transcript_not_found(mocker):
         src.cli.get_transcript(mock_youtube_video)
 
     mock_transcript_generate_transcript.assert_called_once_with(mock_youtube_video)
-
-
-def test_transcripts_disabled(mocker):
-    mock_transcript_get_transcript = mocker.patch("src.transcript.get_transcript")
-    mock_transcript_get_transcript.return_value = (2, "foo")
-    mock_youtube_video = mocker.patch("pytube.YouTube")
-    mock_youtube_video.video_id = "12345"
-    mock_transcript_generate_transcript = mocker.patch(
-        "src.transcript.generate_transcript"
-    )
-
-    mock_transcript_generate_transcript.return_value = (1, "foo")
-
-    with pytest.raises(src.exceptions.TranscriptGenerationFailed):
-        src.cli.get_transcript(mock_youtube_video)
-
-    mock_transcript_generate_transcript.assert_called_once_with(mock_youtube_video)
-
-
-# def test_generation_succeeded(self, mocker):
-#     mock_transcript_get_transcript = mocker.patch("src.transcript.get_transcript")
-#     mock_transcript_get_transcript.return_value = (2, "foo")
-#     mock_youtube_video = mocker.patch("pytube.YouTube")
-#     mock_youtube_video.video_id = "12345"
-#     mock_transcript_generate_transcript = mocker.patch(
-#         "src.transcript.generate_transcript"
-#     )
-#     mock_transcript_generate_transcript.return_value = (0, "foo")
-
-#     src.cli.get_transcript(mock_youtube_video)
-
-#     mock_transcript_generate_transcript.assert_called_once_with(mock_youtube_video)
 
 
 def test_not_implemented_error(mocker):
--- tests/test_transcript.py
+++ tests/test_transcript.py
@@ -40,23 +40,6 @@ class TestGetTranscript:
         mock_transcript_api.get_transcript.assert_called_once_with("foo")
         assert result == 1
 
-    def test_disabled(self, mocker):
-        mock_transcript_api = mocker.patch(
-            "youtube_transcript_api.YouTubeTranscriptApi"
-        )
-        mock_transcript_api.get_transcript.side_effect = (
-            youtube_transcript_api._errors.TranscriptsDisabled("foo")
-        )
-        mock_transcript_api.get_transcript.return_value = [
-            {"text": "bar"},
-            {"text": "baz"},
-        ]
-
-        result, value = get_transcript("foo")
-
-        mock_transcript_api.get_transcript.assert_called_once_with("foo")
-        assert result == 2
-
 
 class TestSplit:
     @pytest.mark.parametrize(
